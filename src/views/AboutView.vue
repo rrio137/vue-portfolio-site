0becc0887eaa1fd17c9e2439355d03ec4e6ec7cb
Ricardo <rich.rios1990@gmail.com>
2025-07-09 16:22:52 -0700

    Added info and changed formatting on the About Me page.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,16 @@
 <template>
     <section class="about">
         <h2>About Me</h2>
-        <p>I'm Rich Rios, an aspiring developer passionate about building web apps using Vue, Java, and Python.</p>
-        <p>I enjoy crafting clean, maintainable code and learning new technologies.</p>
+        <div class="about-card">
+           <p>I'm Rich Rios, an aspiring developer passionate about building web apps using Vue, Java, and Python.
+            I enjoy crafting clean, maintainable code and learning new technologies.</p>
+            <p>I graduated June 2025 with a degree in Computer Science from WGU,
+                and I am eaglerly looking for work opportunities. Thank you for visiting this page!
+            </p>
+            <p>In addition to my computer science degree I have degrees in philosophy (BA) and theology (MA).
+                I am recently married and live in the greater Seattle area of Washington state--go Mariners!
+            </p>
+        </div>
     </section>
 </template>
 
@@ -15,20 +23,29 @@ export default {
 <style scoped>
 .about {
     max-width: 800px;
-    margin: 2rem auto;
-    padding: 1rem;
+    margin: 4rem auto;
+    padding: 2rem 3rem;
+    background: #a88080;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     color: #333;
 }
 
 .about h2 {
-    font-size: 2rem;
+    font-size: 2.5rem;
     margin-bottom: 1rem;
     color: #764ba2;
+}
+.about-card {
+    background-color: #4688a7;
+    border-radius: 12px;
+    padding: 2rem;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.08);
 }
 
 .about p {
     font-size: 1.1rem;
-    line-height: 1.6;
+    line-height: 1.8;
+    margin-bottom: 1rem;
+    font-weight: bold;
 }
 </style>
